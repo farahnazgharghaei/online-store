987001eb7f166e2855854308c9643ae92eb2a776
farahnaz <farahnaz.gharghaei@gmail.com>
2024-01-20 22:25:21 +0330

    fix cors policy error

--- front/src/components/Product/index.jsx
+++ front/src/components/Product/index.jsx
@@ -9,7 +9,7 @@ function ProductList({ handelTotalQty }) {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await fetch("https://fakestoreapi.com/products");
+        const res = await fetch("http://localhost:5000/products");
         const data = await res.json();
         setProducts(data);
       } catch (error) {
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const authRouter = require('./routes/authRouter');
 const indexRouter = require('./routes/indexRourter');
 const productRouter = require('./routes/productRouter');
 const app = express();
+const cors= require("cors")
 
-
+app.use(cors());
 // app.get("/", index);
 // app.get("/login", login);
 // app.get("/signup", signup);
