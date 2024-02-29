68d933fcd3627157d83f2aafc5b9ab967557d049
farahnaz <farahnaz.gharghaei@gmail.com>
2024-02-29 19:49:40 +0330

    fix the env bug

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -1,11 +1,44 @@
-const logger = (req, res, next) => {
-  console.log("url: " + req.url);
-  console.log("method: " + req.method);
-  console.log("params: " + req.params);
-  console.log("query: " + req.query);
-  console.log("body: " + JSON.stringify(req.body));
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 
-  next();
+//  // setup the logger
+//  app.use(morgan("combined", { stream: accessLogStream }));
+
+//  app.get("/", function (req, res) {
+//    res.send("hello, world!");
+//  });
+
+const logger = (req, res, next) => {
+  // console.log("url: " + req.url);
+  // console.log("method: " + req.method);
+  // console.log("params: " + JSON.stringify(req.params));
+  // console.log("query: " + JSON.stringify(req.query));
+  // console.log("body: " + JSON.stringify(req.body));
+  // console.log();
+
+  // console.log(__dirname);
+  // next()
+  const logSysterm = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+    flags: "a",
+  });
+
+  // return morgan(
+  //   function (tokens, req, res) {
+  //     return [
+  //       tokens.method(req, res),
+  //       tokens.url(req, res),
+  //       tokens.status(req, res),
+  //       tokens.res(req, res, "content-length"),
+  //       "-",
+  //       tokens["response-time"](req, res),
+  //       "ms",
+  //     ].join(" ");
+  //   },
+  //   {
+  //     stream: logSysterm,
+  //   }
+  // );
 };
 
 // const user = {
@@ -19,5 +52,4 @@ const logger = (req, res, next) => {
 //     email:"f.110@gmail.com"
 // }
 
-
-module.exports=logger
+module.exports = logger;
